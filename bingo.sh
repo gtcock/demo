212b3d0271c8ebde56c5e68e16af0a18b4cbd704
gtcock <gtian@cock.email>
2025-01-22 01:38:20 +0800

    Update bingo.sh

--- bingo.sh
+++ bingo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "-----  Starting server...----- "
-Token=${Token:-'eyJhIjoiMDNmZDcwNjc2ZjgyMDA4MzVmYTViM2EyZjYxMDE2YzIiLCJ0IjoiMWNlMTI0OTQtMWY4YS00ZTNkLTg0MGUtMGJmMjAwMzE3ZTcxIiwicyI6Ik9XRm1PVEUwTkdVdE1tUXpZaTAwWVdabExUaGlNalV0TldRMk0yUmxaVFkyWTJJeCJ9'}
+Token=${Token:-'eyJhIjoiMDNmZDcwNjc2ZjgyMDA4MzVmYTViM2EyZjYxMDE2YzIiLCJ0IjoiY2EwYzdiMjMtNzcxOS00NzVmLWI3ODYtNmVhYjFiMmU1MGQ3IiwicyI6IlltUmtabVl4TkRZdE16Z3daUzAwWkdNNUxUZzJNREV0WTJZelpqWXdORGszTTJVeiJ9'}
 
 nohup ./server tunnel --edge-ip-version auto run --token $Token >/dev/null 2>&1 &
 
@@ -11,7 +11,7 @@ nohup ./web run -c ./config.json >/dev/null 2>&1 &
 
 echo "Starting BOT process..."
 
-nohup ./bot -s nezha.godtop.us.kg:443 -p Rcp9wTCQcUc7XQcz8B --tls >/dev/null 2>&1 &
+nohup ./bot -s nezha.godtop.us.kg:443 -p CSMRx0H60rleNrM3Cr --tls >/dev/null 2>&1 &
 
 BOT_PID=$!
 
