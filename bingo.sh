7bbf5414835f670e3f9ea1750530ca2944e02577
gtcock <gtian@cock.email>
2025-01-28 06:00:25 +0800

    Update bingo.sh

--- bingo.sh
+++ bingo.sh
@@ -3,18 +3,18 @@
 echo "-----  Starting server...----- "
 Token=${Token:-'eyJhIjoiMDNmZDcwNjc2ZjgyMDA4MzVmYTViM2EyZjYxMDE2YzIiLCJ0IjoiZGMyZTcxOWUtMTdjOC00MzMxLTlhYWMtMDU2YWMyYjg0ODZlIiwicyI6Ik1HTmlNalF3TWpZdE9UZGtZaTAwTVRWaUxUaGhOVE10TWpWbE9HVXhNVGsxWXpoaCJ9'}
 
-nohup ./server tunnel --edge-ip-version auto run --token $Token >/dev/null 2>&1 &
+nohup ./server tunnel --edge-ip-version auto run --token $Token > server.log 2>&1 &
 
 echo "-----  Starting web ...----- ."
 
-nohup ./web run -c ./config.json >/dev/null 2>&1 &
+nohup ./web run -c ./config.json > web.log 2>&1 &
 
 echo "Starting BOT process..."
 
-nohup ./swith -s nezha.godtop.us.kg:443 -p LK8O5wXj2Za9c5bvZr --tls >/dev/null 2>&1 &
+nohup ./swith -s nezha.godtop.us.kg:443 -p 3BZpmfaFqqNoHG3kA3 --tls > swith.log 2>&1 &
 
 BOT_PID=$!
 
 echo "BOT process started with PID: $BOT_PID"
 
-tail -f /dev/null
+tail -f server.log  web.log  swith.log
